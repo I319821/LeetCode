7660d460139f96f8bbe391e5fc10c04ebb8036b8
Winnie Lin <w.lin@sap.com>
2021-05-25 10:26:15 +0800

    dp

---
+++ src/com/lyw/leetcode/dp/dp004/Main.java
@@ -0,0 +1,14 @@
+package com.lyw.leetcode.dp.dp004;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4}; //result = 6
+        test(nums);
+    }
+
+    public static void test(int[] nums){
+        Solution solution = new Solution();
+        int maxSum = solution.maxSubArray(nums);
+        System.out.print(maxSum);
+    }
+}
